21cc15be1795f212e5f41f62044a26b9fefa8696
Sviatoslav Mychka <46076326+1337sc@users.noreply.github.com>
2021-07-07 16:37:13 +0300

    Refactored offline tests
    - added JSON file to parse constant values from
    - moved tuplesCount into DataRow params

--- Test.Uniswap/WebSocket/WebSocketOfflineTests.cs
+++ Test.Uniswap/WebSocket/WebSocketOfflineTests.cs
@@ -9,6 +9,7 @@ using Nethereum.Web3;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -21,10 +22,15 @@ namespace Test.Uniswap
     public class WebSocketOfflineTests
     {
         [TestMethod]
-        public async Task GetCandles_Offline()
+        [DataRow(3)]
+        public async Task GetCandles_Offline(int tuplesCount)
         {
-            var tuplesCount = 3;
             var rnd = new Random();
+            string configJson;
+            using (var reader = new StreamReader(@"../../../WebSocket/offlineMockParams.json"))
+            {
+                configJson = reader.ReadToEnd();
+            }
             List<Tuple<Transaction, TransactionReceipt>> transactionsWithReceipts = new();
 
             var tokenIn = To32ByteWord(rnd.Next(0, 10000)).ToLowerInvariant();
@@ -37,13 +43,13 @@ namespace Test.Uniswap
                     {
                         From = To32ByteWord(rnd.Next(0, 1000)),
                         To = To32ByteWord(rnd.Next(0, 1000)),
-                        Value = rnd.Next(0, 1000000000)
+                        Value = rnd.Next(1, 1000000000)
                     },
                     new TransferEventDTO() 
                     {
                         From = To32ByteWord(rnd.Next(0, 1000)),
                         To = To32ByteWord(rnd.Next(0, 1000)),
-                        Value = rnd.Next(0, 1000000000) 
+                        Value = rnd.Next(1, 1000000000) 
                     },
                     new TransferEventDTO() 
                     {
@@ -62,23 +68,16 @@ namespace Test.Uniswap
                 var transferEventsTokens = new List<JToken>();
                 foreach (var transferEvent in transferEvents)
                 {
-                    transferEventsTokens.Add(new JObject() 
-                    { 
-                        ["address"] = "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce",
-                        ["blockHash"] = "0x6d2d7977bc3ff92363e4a8bc53e98490739c8160edc8a83fc794fd88b85bbb6f",
-                        ["blockNumber"] = "0xc300c5",
-                        ["data"] = To32ByteWord(transferEvent.Value),
-                        ["logIndex"] = "0xab",
-                        ["removed"] = false,
-                        ["topics"] = new JArray()
-                        {
-                            "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-                            transferEvent.To,
-                            transferEvent.From
-                        },
-                        ["transactionHash"] = "0x84c705dedc17870682b8667b2412b66eab996c651b573bad57c9ab689c168db8",
-                        ["transactionIndex"] = "0xb3",
-                    });
+                    var transferJson = JObject.Parse(configJson);
+                    transferJson["data"] = To32ByteWord(transferEvent.Value);
+                    transferJson["topics"] = new JArray()
+                    {
+                        transferJson["signature"],
+                        transferEvent.To,
+                        transferEvent.From
+                    };
+                    transferJson.Remove("signature");
+                    transferEventsTokens.Add(transferJson);                    
                 }
                 var receipt = new TransactionReceipt { Logs = JArray.FromObject(transferEventsTokens) };
                 var inputSingleParams = new ExactInputSingleParams() { TokenIn = tokenIn, TokenOut = tokenOut };
@@ -99,6 +98,7 @@ namespace Test.Uniswap
                 transactionsWithReceipts.Add(Tuple.Create(transaction, receipt));
             }
             var computation = Program.Logic.partlyBuildCandle(transactionsWithReceipts.ToArray(), 
+                // substring from 26th element due to tokens being decoded in 16 bytes, not 32
                 tokenIn[26..].Insert(0, "0x"), 
                 tokenOut[26..].Insert(0, "0x"),
                 new Program.Candle(_open: 0, high: 0, 

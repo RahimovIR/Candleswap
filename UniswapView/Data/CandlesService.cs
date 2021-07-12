a8b211dc51445c09f875b09da79bd023c7ce8808
Sviatoslav Mychka <46076326+1337sc@users.noreply.github.com>
2021-07-12 17:11:57 +0300

    Created Blazor project

---
+++ UniswapView/Data/CandlesService.cs
@@ -0,0 +1,66 @@
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace UniswapView.Data
+{
+    public class CandlesService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public async Task<FSharpBack.DBCandle[]> GetCandlesAsync(string symbol, int periodSeconds, 
+            long? startTime = default, long? endTime = default, int? limit = default)
+        {
+            var client = new HttpClient();
+            var queryParams = new NameValueCollection
+            {
+                { "symbol", symbol },
+                { "periodSeconds", periodSeconds.ToString() }
+            };
+            if (startTime.HasValue)
+            {
+                queryParams.Add("startTime", startTime.ToString());
+            }
+            if (endTime.HasValue)
+            {
+                queryParams.Add("endTime", endTime.ToString());
+            }
+            if (limit.HasValue)
+            {
+                queryParams.Add("limit", limit.ToString());
+            }
+            var request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://localhost:5001/api/candles" + ToQueryString(queryParams)),
+            };
+
+            var response = await client.SendAsync(request);
+            var responseToken = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync(responseToken, typeof(FSharpBack.DBCandle[])) 
+                as FSharpBack.DBCandle[];
+        }
+
+        private string ToQueryString(NameValueCollection nvc)
+        {
+            var array = (
+                from key in nvc.AllKeys
+                from value in nvc.GetValues(key)
+                select string.Format(
+            "{0}={1}",
+            HttpUtility.UrlEncode(key),
+            HttpUtility.UrlEncode(value))
+                ).ToArray();
+            return "?" + string.Join("&", array);
+        }
+    }
+}
---
+++ UniswapView/wwwroot/js/chart.js
@@ -0,0 +1,15 @@
+﻿google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart(rawData) {
+    console.log(rawData);
+    var data = google.visualization.arrayToDataTable(rawData, true);
+    
+    var options = {
+        legend: 'none'
+    };
+
+    var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
